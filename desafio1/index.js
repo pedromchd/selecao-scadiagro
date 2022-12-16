4fb8db2c7ebe331b128ec5393a18f0a02a123e94
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2022-12-16 17:39:19 -0300

    desafio1

--- desafio1/index.js
+++ desafio1/index.js
@@ -1,36 +1,30 @@
 function validarCNPJ(cnpj) {
-    var retorno = true;
-    cnpj = cnpj.replace('.', '').replace('/', '').replace('-', '');
-    if (cnpj.length != 14)
-        retorno = false;
-    if (cnpj == "00000000000000" || cnpj == "11111111111111" || cnpj == "22222222222222" || cnpj == "33333333333333" ||
-        cnpj == "44444444444444" || cnpj == "55555555555555" || cnpj == "66666666666666" || cnpj == "77777777777777" ||
-        cnpj == "88888888888888" || cnpj == "99999999999999")
-        retorno = false;
-    var tamanho = cnpj.length - 2;
-    var numeros = cnpj.substring(0, tamanho);
-    var dig = cnpj.substring(tamanho);
-    var soma = 0;
-    var pos = tamanho - 7;
-    for (var i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-        if (pos < 2)
-            pos = 9;
+    cnpj = cnpj.match(/[0-9]/g).join('');
+    if (cnpj === '00000000000000' || cnpj === '11111111111111' ||
+        cnpj === '22222222222222' || cnpj === '33333333333333' ||
+        cnpj === '44444444444444' || cnpj === '55555555555555' ||
+        cnpj === '66666666666666' || cnpj === '77777777777777' ||
+        cnpj === '88888888888888' || cnpj === '99999999999999' ||
+        cnpj.length !== 14) {
+        return false;
     }
-    var resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado != parseInt(dig.charAt(0)))
-        retorno = false;
-    tamanho = tamanho + 1;
-    numeros = cnpj.substring(0, tamanho);
-    soma = 0;
-    pos = tamanho - 7;
-    for (var i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-        if (pos < 2)
-            pos = 9;
+    try {
+        const temp = cnpj.split('').map((n) => parseInt(n)).reverse();
+        let peso = 0;
+        let soma = temp.slice(2).reduce((soma, numr) => soma + numr * ((peso++ % 8) + 2), 0);
+        let resu = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+        if (resu !== temp[1]) {
+            return false;
+        }
+        peso = 0;
+        soma = temp.slice(1).reduce((soma, numr) => soma + numr * ((peso++ % 8) + 2), 0);
+        resu = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+        if (resu !== temp[0]) {
+            return false;
+        }
     }
-    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado != parseInt(dig.charAt(1)))
-        retorno = false;
-    return retorno;
+    catch (erro) {
+        return false;
+    }
+    return true;
 }
--- desafio1/index.ts
+++ desafio1/index.ts
@@ -1,32 +1,57 @@
 function validarCNPJ(cnpj: string): boolean {
-  let retorno: boolean = true;
-  cnpj = cnpj.replace('.', '').replace('/', '').replace('-', '');
-  if (cnpj.length != 14) retorno = false;
-  if (cnpj == "00000000000000" || cnpj == "11111111111111" || cnpj == "22222222222222" || cnpj == "33333333333333" ||
-    cnpj == "44444444444444" || cnpj == "55555555555555" || cnpj == "66666666666666" || cnpj == "77777777777777" ||
-    cnpj == "88888888888888" || cnpj == "99999999999999")
-    retorno = false;
-  let tamanho: number = cnpj.length - 2
-  let numeros: string = cnpj.substring(0, tamanho);
-  let dig: string = cnpj.substring(tamanho);
-  let soma: number = 0;
-  let pos: number = tamanho - 7;
-  for (let i: number = tamanho; i >= 1; i--) {
-    soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-    if (pos < 2) pos = 9;
+  // Não há necessidade de atribuir um valor booleano a uma variável e retorná-lo,
+  // você pode diretamente retornar "true" ou "false" dependedo da condição.
+
+  // Usar uma expressão regular nesse caso é muito mais eficiente e prático,
+  // além de não permitir brechas para outros símbolos dentro da variável:
+  cnpj = cnpj.match(/[0-9]/g).join('');
+
+  // Priorizar um comparador mais específico, e seguir a sintaxe padrão do if.
+  if (
+    cnpj === '00000000000000' || cnpj === '11111111111111' ||
+    cnpj === '22222222222222' || cnpj === '33333333333333' ||
+    cnpj === '44444444444444' || cnpj === '55555555555555' ||
+    cnpj === '66666666666666' || cnpj === '77777777777777' ||
+    cnpj === '88888888888888' || cnpj === '99999999999999' ||
+    cnpj.length !== 14
+  ) {
+    return false;
   }
-  let resultado: number = soma % 11 < 2 ? 0 : 11 - soma % 11;
-  if (resultado != parseInt(dig.charAt(0)))
-    retorno = false;
-  tamanho = tamanho + 1;
-  numeros = cnpj.substring(0, tamanho);
-  soma = 0;
-  pos = tamanho - 7;
-  for (let i: number = tamanho; i >= 1; i--) {
-    soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-    if (pos < 2) pos = 9;
+
+  // try...catch ajuda a lidar com eventuais problemas de execução do código
+  try {
+    // Com uma variável temporária, podemos transformar o cnpj em uma array de números.
+    // O reverse() serve para fazer a interação com o peso.
+    const temp: number[] = cnpj.split('').map((n) => parseInt(n)).reverse();
+    // Se dispensa o uso de substring() para pegar os últimos 2 números do cnpj.
+
+    // Aqui é um exemplo do método reduce(),
+    // ele permite fazer um somatório consistente dos números.
+    let peso: number = 0;
+    let soma: number = temp.slice(2).reduce(
+      (soma, numr) => soma + numr * ((peso++ % 8) + 2), 0
+    );
+    // A expressão "peso++ % 8 + 2" autoincrementa valores em "peso",
+    // e retorna um valor seguindo o padrão "2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, ...",
+    // tais valores servem pro cálculo do dígito verificador do cnpj.
+
+    // Seguindo a sintaxe padrão do if
+    let resu: number = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+    if (resu !== temp[1]) {
+      return false;
+    }
+
+    // Repetindo os algoritmos
+    peso = 0;
+    soma = temp.slice(1).reduce(
+      (soma, numr) => soma + numr * ((peso++ % 8) + 2), 0
+    );
+    resu = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+    if (resu !== temp[0]) {
+      return false;
+    }
+  } catch (erro) {
+    return false;
   }
-  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-  if (resultado != parseInt(dig.charAt(1))) retorno = false;
-  return retorno;
+  return true;
 }
